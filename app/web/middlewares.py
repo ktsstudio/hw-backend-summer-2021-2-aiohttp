f74e5976c21f35c1220e309d0cdad3c4acb47d8c
Станислав Цаплев <stanislav.tsaplev@gmail.com>
2022-02-05 20:31:03 +0300

    refactor http text statuses in response

--- app/web/middlewares.py
+++ app/web/middlewares.py
@@ -19,16 +19,6 @@ from app.web.utils import error_json_response
 if typing.TYPE_CHECKING:
     from app.web.app import Application, Request
 
-HTTP_ERROR_CODES = {
-    400: "bad_request",
-    401: "unauthorized",
-    403: "forbidden",
-    404: "not_found",
-    405: "not_implemented",
-    409: "conflict",
-    500: "internal_server_error",
-}
-
 
 @middleware
 async def error_handling_middleware(request: "Request", handler):
@@ -38,56 +28,48 @@ async def error_handling_middleware(request: "Request", handler):
     except (HTTPBadRequest, HTTPUnprocessableEntity) as e:
         return error_json_response(
             http_status=400,
-            status=HTTP_ERROR_CODES[400],
             message=e.reason,
             data=json.loads(e.text),
         )
     except HTTPUnauthorized as e:
         return error_json_response(
             http_status=401,
-            status=HTTP_ERROR_CODES[401],
             message=e.reason,
             data=json.loads(e.text),
         )
     except HTTPForbidden as e:
         return error_json_response(
             http_status=403,
-            status=HTTP_ERROR_CODES[403],
             message=e.reason,
             data=json.loads(e.text),
         )
     except HTTPNotFound as e:
         return error_json_response(
             http_status=404,
-            status=HTTP_ERROR_CODES[404],
             message=e.reason,
             data=json.loads(e.text),
         )
     except HTTPMethodNotAllowed as e:
         return error_json_response(
             http_status=405,
-            status=HTTP_ERROR_CODES[405],
             message=e.reason,
             data=json.loads(e.text),
         )
     except HTTPConflict as e:
         return error_json_response(
             http_status=409,
-            status=HTTP_ERROR_CODES[409],
             message=e.reason,
             data=json.loads(e.text),
         )
     except HTTPInternalServerError as e:
         return error_json_response(
             http_status=500,
-            status=HTTP_ERROR_CODES[500],
             message=e.reason,
             data=json.loads(e.text),
         )
     except Exception as e:
         return error_json_response(
             http_status=500,
-            status=HTTP_ERROR_CODES[500],
             message=e.__doc__,
             data={ "args": e.args },
         )
--- app/web/utils.py
+++ app/web/utils.py
@@ -4,6 +4,16 @@ from aiohttp.web import json_response as aiohttp_json_response
 from aiohttp.web_response import Response
 
 
+HTTP_ERROR_CODES = {
+    400: "bad_request",
+    401: "unauthorized",
+    403: "forbidden",
+    404: "not_found",
+    405: "not_implemented",
+    409: "conflict",
+    500: "internal_server_error",
+}
+
 def json_response(data: Any = None, status: str = "ok") -> Response:
     if data is None:
         data = {}
@@ -14,11 +24,17 @@ def json_response(data: Any = None, status: str = "ok") -> Response:
         }
     )
 
-
 def error_json_response(
     http_status: int,
-    status: str = "error",
+    status: Optional[str] = None,
     message: Optional[str] = None,
     data: Optional[dict] = None,
-):
-    raise NotImplementedError
+) -> Response:
+    return aiohttp_json_response(
+        status=http_status,
+        data={
+            "status": status or HTTP_ERROR_CODES[http_status],
+            "message": message,
+            "data": data,
+        }
+    )
